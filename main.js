16deacea6dcb157afca9ee5ea9768adbd043f02c
idanhom <76005756+idanhom@users.noreply.github.com>
2024-01-02 16:49:39 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         LinkedIn Recruiter Enhanced Navigation
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Use keyboard to navigate and interact with profiles in LinkedIn Recruiter
+// @author       YourName
+// @grant        none
+// @match        https://www.linkedin.com/talent/hire/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to simulate button click
+    function clickButton(selector) {
+        const button = document.querySelector(selector);
+        if (button) {
+            button.click();
+        }
+    }
+
+    // Keyboard event listener
+    document.addEventListener('keydown', function(e) {
+        // Check if the focus is on an input, textarea, or any element that is contenteditable
+        if (document.activeElement && (document.activeElement.tagName.toLowerCase() === 'input' || document.activeElement.tagName.toLowerCase() === 'textarea' || document.activeElement.isContentEditable)) {
+            return; // If so, do not execute hotkey actions
+        }
+
+        switch (e.key) {
+            case 'a': // backwards
+                clickButton('a[rel="prev"]');
+                break;
+            case 's': // forwards
+                clickButton('a[rel="next"]');
+                break;
+            case 'd': // save to pipeline
+                clickButton('.save-to-pipeline__button.save-to-pipeline__button--with-trigger');
+                break;
+            case 'f': // hide
+                clickHideButton();
+                break;
+            case ' ': // send a message
+                e.preventDefault();
+                clickButton('button[data-live-test-component="message-icon-btn"]');
+                break;
+        }
+    });
+
+    // Specific function to handle the hide button
+    function clickHideButton() {
+        var hideButton = document.querySelector('button[data-live-test-component="hide-btn"]');
+        if (hideButton) {
+            hideButton.click();
+        }
+    }
+})();
