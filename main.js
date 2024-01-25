11327efc6aaf505a07fde9d1858d690e3c1de7e5
idanhom <76005756+idanhom@users.noreply.github.com>
2024-01-25 10:12:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LinkedIn Recruiter Enhanced Navigation
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Use keyboard to navigate and interact with profiles in LinkedIn Recruiter
 // @author       Oscar
 // @grant        none
@@ -21,7 +21,7 @@
 
     // Function to scroll down the profile container by a fixed amount
     function scrollDownProfile() {
-        const scrollAmount = 300; // Amount of pixels to scroll down
+        const scrollAmount = 300;
         const profileContainer = document.querySelector('#title-pagination-header-text > div > div.profile.profile-slidein__profile > div.profile__main-container');
         if (profileContainer) {
             profileContainer.scrollBy(0, scrollAmount);
@@ -30,21 +30,31 @@
 
     // Function to scroll up the profile container by a fixed amount
     function scrollUpProfile() {
-        const scrollAmount = 300; // Amount of pixels to scroll up
+        const scrollAmount = 300;
         const profileContainer = document.querySelector('#title-pagination-header-text > div > div.profile.profile-slidein__profile > div.profile__main-container');
         if (profileContainer) {
             profileContainer.scrollBy(0, -scrollAmount);
         }
     }
 
-function expandAllSkills() {
-    const skillButtonSelector = '.expandable-list__button[aria-label^="Show all"]';
-    const skillButton = document.querySelector(skillButtonSelector);
+    // Function to expand all skills
+    function expandAllSkills() {
+        const skillButtonSelector = '.expandable-list__button[aria-label^="Show all"]';
+        const skillButton = document.querySelector(skillButtonSelector);
 
-    if (skillButton) {
-        skillButton.click();
+        if (skillButton) {
+            skillButton.click();
+        }
     }
-}
+
+    // Function to expand work experiences
+    function expandWorkExperiences() {
+        const expandMoreButton = document.querySelector('button[aria-label="See more positions"]');
+
+        if (expandMoreButton) {
+            expandMoreButton.click();
+        }
+    }
 
     // Specific function to toggle hide/unhide button
     function clickHideButton() {
@@ -70,55 +80,55 @@ function expandAllSkills() {
         }
     }
 
+    // Keyboard event listener for handling key presses
+    document.addEventListener('keydown', function(e) {
+        // Check if the focus is on an input, textarea, or any element that is contenteditable
+        if (document.activeElement && (document.activeElement.tagName.toLowerCase() === 'input' || document.activeElement.tagName.toLowerCase() === 'textarea' || document.activeElement.isContentEditable)) {
+            return; // Do not execute hotkey actions if in an editable element
+        }
 
-// Keyboard event listener for handling key presses
-document.addEventListener('keydown', function(e) {
-    // Check if the focus is on an input, textarea, or any element that is contenteditable
-    if (document.activeElement && (document.activeElement.tagName.toLowerCase() === 'input' || document.activeElement.tagName.toLowerCase() === 'textarea' || document.activeElement.isContentEditable)) {
-        return; // Do not execute hotkey actions if in an editable element
-    }
-
-    // Handle different key presses
-    switch (e.key) {
-        case 'a': // Navigate backwards
-        case 'j': // Navigate backwards (right hand)
-            clickButton('a[rel="prev"]');
-            break;
-        case 's': // Navigate forwards
-        case 'k': // Navigate forwards (right hand)
-            clickButton('a[rel="next"]');
-            break;
-        case 'd': // Save to pipeline
-        case 'l': // Save to pipeline (right hand)
-            clickButton('.save-to-pipeline__button.save-to-pipeline__button--with-trigger');
-            break;
-        case 'f': // Hide/Unhide profile
-        case 'ö': // Hide/Unhide profile (right hand)
-            clickHideButton();
-            break;
-        case ' ': // Send a message
-            e.preventDefault();
-            clickButton('button[data-live-test-component="message-icon-btn"]');
-            break;
-        case 'v': // Scroll down the profile
-        case 'm': // Scroll down the profile (right hand)
-            e.preventDefault();
-            scrollDownProfile();
-            break;
-        case 'c': // Scroll up the profile
-        case 'n': // Scroll up the profile (right hand)
-            e.preventDefault();
-            scrollUpProfile();
-            break;
-        case 'g': // Toggle "See More" and "See Less"
-        case 'ä': // Toggle "See More" and "See Less" (right hand)
-            e.preventDefault();
-            toggleSeeMoreLess();
-            break;
-        case 'r': // Expand all skills
-            e.preventDefault();
-            expandAllSkills();
-            break;
+        // Handle different key presses
+        switch (e.key) {
+            case 'a': // Navigate backwards
+            case 'j': // Navigate backwards (right hand)
+                clickButton('a[rel="prev"]');
+                break;
+            case 's': // Navigate forwards
+            case 'k': // Navigate forwards (right hand)
+                clickButton('a[rel="next"]');
+                break;
+            case 'd': // Save to pipeline
+            case 'l': // Save to pipeline (right hand)
+                clickButton('.save-to-pipeline__button.save-to-pipeline__button--with-trigger');
+                break;
+            case 'f': // Hide/Unhide profile
+            case 'ö': // Hide/Unhide profile (right hand)
+                clickHideButton();
+                break;
+            case ' ': // Send a message
+                e.preventDefault();
+                clickButton('button[data-live-test-component="message-icon-btn"]');
+                break;
+            case 'v': // Scroll down the profile
+            case 'm': // Scroll down the profile (right hand)
+                e.preventDefault();
+                scrollDownProfile();
+                break;
+            case 'c': // Scroll up the profile
+            case 'n': // Scroll up the profile (right hand)
+                e.preventDefault();
+                scrollUpProfile();
+                break;
+            case 'g': // Toggle "See More" and "See Less"
+            case 'ä': // Toggle "See More" and "See Less" (right hand)
+                e.preventDefault();
+                toggleSeeMoreLess();
+                break;
+            case 'r': // Expand all skills and work experiences
+                e.preventDefault();
+                expandAllSkills();
+                expandWorkExperiences();
+                break;
         }
     });
 
